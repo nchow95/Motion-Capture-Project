fc145e7ad869b225833aca616fe0f171cf7e1957
nchow95 <nchow@purdue.edu>
2019-04-23 00:00:33 -0400

    finished gui code
    I now need to add the keyboard control code. In all honesty I am trying to avoid the problem with the serial port. So I am going to work on control the ship without the IMU :D

---
+++ Assets/code/master.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class master : MonoBehaviour
+{
+    public GameObject ship;
+    public GameObject data_in;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data_in = GameObject.Find("data_obj");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/code/menu.cs
+++ Assets/code/menu.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    public Button begin;
-    public GameObject data;
+    public data data_obj;
     public Toggle keyboard;
     public Toggle controller;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,43 @@ public class menu : MonoBehaviour
 
     public void OnClick()
     {
-        Debug.Log("Begin Game");
+        SceneManager.LoadScene("scene_1");
+    }
+
+    public void OnToggle(Toggle change)
+    {
+        if(change.name == "Keyboard")
+        {
+            Debug.Log("Keyboard Toggled");
+            if (change.isOn)
+            {
+                controller.isOn = false;
+                data_obj.controller = false;
+                data_obj.keyboard = true;
+            }
+            else
+            {
+                controller.isOn = true;
+                data_obj.controller = false;
+                data_obj.keyboard = true;
+
+            }
+        }
+        if(change.name == "Controller")
+        {
+            Debug.Log("Controller Toggled");
+            if (change.isOn)
+            {
+                keyboard.isOn = false;
+                data_obj.keyboard = false;
+                data_obj.controller = true;
+            }
+            else
+            {
+                keyboard.isOn = true;
+                data_obj.keyboard = true;
+                data_obj.controller = false;
+            }
+        }
     }
 }
